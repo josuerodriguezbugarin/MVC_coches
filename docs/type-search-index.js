eb2930d5f3fa199519f5306b29be656a727639e2
josue <josuerodriguezbuarin@danielcastelao.org>
2026-05-26 09:14:59 +0200

    JavaDoc 3

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"<Unnamed>","l":"Coche"},{"p":"<Unnamed>","l":"Controller"},{"p":"<Unnamed>","l":"Model"},{"p":"<Unnamed>","l":"View"}];updateSearchResults();
